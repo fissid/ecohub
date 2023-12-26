bd88fc365cc4f45baf7f28acec3cce055a52c5d2
fissid <fissid.ss@gmail.com>
2023-12-26 11:47:01 +0300

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Quiz from "./Quiz";
 import { useEffect, useState } from "react";
 import Suggestion from "./Suggestions";
 import UserProfile from "./UserProfile";
+import Projects from "./Projects";
 const menuBtns = [
   {
     id: 0,
@@ -67,19 +68,48 @@ function App() {
     });
   }
 
+  let tag;
+  let profileNav;
+  sideMenu.forEach((each) => {
+    if (each.selected) {
+      switch (each.id) {
+        case 0:
+          tag = <Explore className="middle"></Explore>;
+          profileNav = <ProfileNavbar who="me"></ProfileNavbar>;
+          break;
+        case 1:
+          tag = <Projects className="middle"></Projects>;
+          profileNav = <ProfileNavbar who="me"></ProfileNavbar>;
+          break;
+        case 2:
+          tag = <Suggestion className="middle"></Suggestion>;
+          profileNav = <ProfileNavbar who="me"></ProfileNavbar>;
+          break;
+        case 3:
+          tag = <Quiz className="middle"></Quiz>;
+          profileNav = null;
+          break;
+        default:
+      }
+    }
+  });
+
   return (
     <main className="app">
       <section className="top">
         <MainNavbar menu={sideMenu} onClick={menuClickHandler}></MainNavbar>
-        <Explore className={`middle ${!sideMenu[0].selected && "none"}`}></Explore>
+        {tag}
+        {/* <Explore className={`middle ${!sideMenu[0].selected && "none"}`}></Explore> */}
         {/* whne quiz is shown, profileNavbar is invisible */}
-        <Quiz className={`middle ${!sideMenu[3].selected && "none"}`}></Quiz>
-        <Suggestion className={`middle ${!sideMenu[2].selected && "none"}`}></Suggestion>
+        {/* <Quiz className={`middle ${!sideMenu[3].selected && "none"}`}></Quiz> */}
+        {/* <Suggestion className={`middle ${!sideMenu[2].selected && "none"}`}></Suggestion> */}
+
         <UserProfile className="none middle"></UserProfile>
+        {profileNav}
         {/* without who is for user */}
-        <ProfileNavbar className={`${sideMenu[3].selected ? "none" : !sideMenu[0].selected ? "" : "none"}`}></ProfileNavbar>
+        {/* <ProfileNavbar className={`${sideMenu[3].selected ? "none" : !sideMenu[0].selected ? "" : "none"}`}></ProfileNavbar> */}
         {/* with who is for me */}
-        <ProfileNavbar className={`${sideMenu[3].selected ? "none" : sideMenu[0].selected ? "" : "none"}`} who="me"></ProfileNavbar>
+        {/* <ProfileNavbar className={`${sideMenu[3].selected ? "none" : sideMenu[0].selected ? "" : "none"}`} who="me"></ProfileNavbar> */}
       </section>
     </main>
   );

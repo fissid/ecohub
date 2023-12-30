4b8fc55cd715d5bf56c63f99af0269d258968c24
fissid <fissid.ss@gmail.com>
2023-12-30 17:15:28 +0300

    updates

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,37 +2,48 @@ import { useState } from "react";
 import "../scss/Login.scss";
 import image from "../img/minim/dashboard.png";
 import mebis from "../img/minim/Mebis.png";
-import logoWhite from "../img/logoWhite.PNG";
-
-export default function Login() {
+export default function Login({ userLoginHandler }) {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
+  const [approved, setApproved] = useState(false);
+  // saeed.salehi@std.medipol.edu.tr
+  function loginSubmit(e) {
+    e.preventDefault();
+    setApproved((prev) => !prev);
+    userLoginHandler();
+  }
+  function signUpBtnHandler() {}
   return (
     <div className="login">
       <div className="box">
-        <form>
+        <form onSubmit={(e) => loginSubmit(e)}>
           <div className="header">
             <h3>Login</h3>
             <p>
-              Doesn't have an account yet? <button>Sign Up</button>
+              Doesn't have an account yet? <input type="button" value="Sign up" onClick={signUpBtnHandler} />
             </p>
           </div>
           <div className="email">
-            <label htmlFor="email">Email Address:</label>
-            <input id="email" name="email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <label htmlFor="email" className={`${!approved && "warn"}`}>
+              Email Address:
+            </label>
+            <div className={`email__container ${!approved && "not-approved"}`}>
+              <input id="email" name="email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
+              <input type="text" placeholder="@std.medipol.edu.tr" className="unavaialbe" disabled />
+            </div>
           </div>
 
           <div className="pass">
-            <label htmlFor="password">Password:</label>
-            <input id="password" name="password" type="text" value={pass} onChange={(e) => setPass(e.target.value)} required />
+            <label htmlFor="password" className={`${!approved && "warn"}`}>
+              Password:
+            </label>
+            <input className={`${!approved && "not-approved"}`} id="password" name="password" type="text" value={pass} onChange={(e) => setPass(e.target.value)} required />
           </div>
 
           <div className="footer">
             <button type="submit">Login</button>
           </div>
-          <div className="footer-line">
-            <p>OR</p>
-          </div>
+          <div className="footer-line">{approved ? <p>OR</p> : <p className="warn">Email or password is incorrect</p>}</div>
           <div className="footer-foot">
             <button>
               <p>Login With MEBİS</p> <img src={mebis} alt="" />
